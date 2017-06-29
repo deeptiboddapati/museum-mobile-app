d8faa3d236e8c3a0f7d35831e9adb4c201df39e3
deeptiboddapati <deeptiboddapati@gmail.com>
2017-06-29 10:00:15 -0500

    Created plugin for the site.

---
+++ WordPress Plugin/mobile-interpretive-app.php
@@ -0,0 +1,10 @@
+<?php
+/**
+Plugin Name: Museum Mobile Interpretive App
+Plugin URI: http://deeptiboddapati.com
+Description: Adds the capability of turning your WordPress install into a mobile backend
+Version: .1
+Author: Deepti Boddapati
+Author URI:  http://deeptiboddapati.com
+Text Domain: DB
+*/
